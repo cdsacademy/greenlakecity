b495ce203844632a039932382a7ec1b216691079
Cano Carlos <Carlos.Cano@hpecds.com>
2023-04-21 10:37:23 +0200

    new coal endpoint

--- kafka_daily_reporter/main.py
+++ kafka_daily_reporter/main.py
@@ -1,5 +1,6 @@
 from kafka import KafkaProducer
 from datetime import datetime, timedelta
+import traceback
 import json
 import time
 import os
@@ -9,6 +10,8 @@ try:
     KAFKA_SERVERS = f"{os.environ.get('KAFKA_HOST')}:{os.environ.get('KAFKA_PORT')}"
     START_DATE = datetime.strptime(os.environ.get('START_DATE'), '%d/%m/%Y')
     END_DATE = datetime.strptime(os.environ.get('END_DATE'), '%d/%m/%Y')
+    DAY_SIMULATION_INTERVAL_SECONDS = int(os.environ.get('DAY_SIMULATION_INTERVAL_SECONDS'))
+
     time.sleep(20)
     datasets = {}
     for filename in os.listdir(DATA_DIRECTORY):
@@ -20,12 +23,15 @@ try:
     while current_date <= END_DATE:
         str_current_date = current_date.strftime('%d/%m/%Y')
         for topic, dataset in datasets.items():
-            message = json.dumps({str_current_date: dataset[str_current_date]})
-            producer.send(topic, message.encode('utf-8'))
-        time.sleep(3)
+            print(json.dumps({str_current_date: topic}))
+            if (str_current_date in dataset):
+                message = json.dumps({str_current_date: dataset[str_current_date]})
+                producer.send(topic, message.encode('utf-8'))
+        time.sleep(DAY_SIMULATION_INTERVAL_SECONDS)
         current_date += timedelta(days=1)
-except:
-    print ('Found unexpected error')
+except Exception as e:
+    traceback.print_exc()
+    print ('Found unexpected error', e)
 
 # kafka-console-consumer.sh --bootstrap-server kafka:9092 --topic hydrodataWAT001 --from-beginning
 
